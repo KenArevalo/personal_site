ababaccc752f3bbe8c75ab79b8b6d841d0dd6d51
KenArevalo <55779875+KenArevalo@users.noreply.github.com>
2020-09-17 17:57:46 -0700

    Update FirstContainer.jsx

--- FirstContainer.jsx
+++ FirstContainer.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import './App.css';
+// import FadeIn from "react-fade-in";
+
 
 function firstContainer() {
+
+  window.onload = function() {
+    window.setTimeout(fadeout, 3000); //3 seconds
+  }
+  
+  function fadeout() {
+    document.getElementById('Dev-font').style.opacity = '0';
+  }
+
     return (
       <div className="Header">
         <header className="App-header">
-          <div class="K2-logo">
-            <img src="/public/K2_Logo_1.png" alt="logo"/>
+          <div className="K2-logo">
+            <img src="../K2_Logo_1.png" alt="K2"/>
           </div>
-          <p className="Dev-font">
+          {/* <FadeIn> */}
+          <p id="Dev-font">
             |DEVELOPMENT TECHNOLOGIES|
            </p>
+           {/* </FadeIn> */}
         </header>
       </div>
     );
   }
 
 export default firstContainer;
+
+// Primary source code below
+
+// window.onload = function() {
+//   window.setTimeout(fadeout, 8000); //8 seconds
+// }
+
+// function fadeout() {
+//   document.getElementById('fadeout').style.opacity = '0';
+// }
+
+// eslint-disable-next-line
+{/* <div id="Dev-font">
+|DEVELOPMENT TECHNOLOGIES|
+</div> */}
